d17266260cc4b28a9c5ffc43e1cddff7d9030432
vivitt <viviviyanez@gmail.com>
2024-04-24 21:41:09 +0200

    add world face animation

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const h1 = document.getElementsByTagName("h1")[0];
 const hero = document.createElement("div");
 hero.classList.add("hero");
 header.append(hero);
-const svgContainer = document.createElement("div");
 
-svgContainer.classList.add("world-container");
+const worldContainer = document.createElement("div");
+worldContainer.classList.add("world-container");
 
-const svgCode = `<svg viewBox="-35 -50 915 915">
+const svgCode = `<svg viewBox="-35 -50 915 500">
 <path id="curve" d="M3.02245 482.5C-48.9776 -133 883 -185 842.522 482.5" />
 <text width="500" >
   <textPath xlink:href="#curve" startOffset="80">
@@ -19,23 +19,63 @@ const svgCode = `<svg viewBox="-35 -50 915 915">
 </text>
 </svg>`;
 
-svgContainer.innerHTML = svgCode;
-hero.appendChild(svgContainer);
-h1.classList.add("sr-only");
-console.log(h1);
+worldContainer.innerHTML = svgCode;
 
+/* HERO Illustration */
+const worldIllustration = document.createElement("div");
+worldIllustration.classList.add("world");
+
+/* World */
 const worldSvg = document.createElement("img");
 worldSvg.setAttribute("src", "mundo.svg");
+worldSvg.setAttribute("alt", "Smiling Planet Earth Illustration");
 worldSvg.classList.add("mundo");
+worldIllustration.append(worldSvg);
 
-hero.append(worldSvg);
+/* World face */
+const faceContainer = document.createElement("div");
+faceContainer.classList.add("face-container");
+const faceSvg = document.createElement("div");
+faceSvg.classList.add("cara");
+faceContainer.append(faceSvg);
+worldIllustration.append(faceContainer);
+
+worldContainer.append(worldIllustration);
+hero.appendChild(worldContainer);
+h1.classList.add("sr-only");
 
 document.addEventListener("DOMContentLoaded", function () {
+  const section = document.getElementsByTagName("section")[0];
   window.onscroll = function () {
-    if (window.scrollY > 30) {
-      worldSvg.classList.add("small");
+    if (window.scrollY > 50) {
+      section.classList.add("animate-section");
+      worldIllustration.classList.add("small");
+      faceSvg.classList.add("animate-face");
     } else {
-      worldSvg.classList.remove("small");
+      section.classList.remove("animate-section");
+      worldIllustration.classList.remove("small");
+      faceSvg.classList.remove("animate-face");
     }
   };
+});
+
+const pos = { x: 0, y: 0 };
+
+const root = document.querySelector(":root");
+
+const saveCursorPosition = function (x, y) {
+  pos.x = (x / window.innerWidth).toFixed(2);
+  pos.y = (y / window.innerHeight).toFixed(2);
+  faceSvg.style.transform = `translate(calc(-50% + 100px * ${pos.x}), calc(-50% + 100px * ${pos.y}))`;
+};
+
+document.addEventListener("mousemove", (e) => {
+  saveCursorPosition(e.clientX, e.clientY);
+  if (e.clientX < 400) {
+    faceSvg.classList.add("look-left");
+    faceSvg.classList.remove("look-right");
+  } else {
+    faceSvg.classList.add("look-right");
+    faceSvg.classList.remove("look-left");
+  }
 });
